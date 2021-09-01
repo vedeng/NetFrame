2c5296f2adb06edb013b7179064ec259ee95e56b
Bendan <1017161726@qq.com>
2021-09-02 00:19:18 +0800

    BaseResponse change field type

--- netLib/src/main/java/com/netlib/BaseResponse.kt
+++ netLib/src/main/java/com/netlib/BaseResponse.kt
@@ -1,10 +1,10 @@
 package com.netlib
 
 open class BaseResponse {
-    var code: String? = null
-    var status: String? = null
-    var message: String? = null
-    var success: Boolean? = null
+    open var code: String? = null
+    open var status: String? = null
+    open var message: String? = null
+    open var success: Boolean? = null
 
     override fun toString(): String {
         return "BaseResponse(code=$code, status=$status, message=$message, success=$success)"
